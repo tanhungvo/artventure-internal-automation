786e0658b3d723630e6aa55e53ae848638e2cb84
tanhungvo <tanhungvo@localhost>
2023-11-20 11:00:21 +0700

    Open recipes page

--- src/selenium/Topic_01_CheckEnvironments.java
+++ src/selenium/Topic_01_CheckEnvironments.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;import org.seleniumhq.jetty9.security.AbstractUserAuthentication;
 import org.testng.annotations.AfterClass;
 
 @Test
@@ -52,11 +52,24 @@ public class Topic_01_CheckEnvironments {
 				break;
 			}		    
 		}
+		driver.switchTo().window(mainWindowHandle);
 	}
 
+	public void TC_02_OpenRecipesPage() throws InterruptedException {
+		driver.findElement(By.xpath("//*[starts-with(text(),'Recipes')]")).click();
+		Thread.sleep(5000);
+		
+	}
+	
+	public void TC_03_Check_Outpainting_LCM() {
+		driver.findElement(By.xpath("//p[1][starts-with(text(),'Outpainting LCM')]")).click();
+		
+		driver.findElement(By.xpath("//p[@class='text-atherGray-300 text-center'][text()='Upload images or Drag & Drop ']")).click();
+	}
+	
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+		//driver.quit();
 	}
 
 }
